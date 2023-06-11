120fb6cce2938099befa155347a7834b15b4ab4b
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-06-11 18:04:55 -0300

    tp1 citas terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Form from './Components/Form'
+import Cita from './Components/Cita'
 
 function App() {
   return (
@@ -12,29 +13,10 @@ function App() {
         </div>
         <div className="one-half column">
           <h2>Administra tus citas</h2>
-          <div className="cita">
-            <p>Mascota: <span>Blacky</span></p>
-            <p>Dueño: <span>Jere</span></p>
-            <p>Fecha: <span>2023-05-08</span></p>
-            <p>Hora: <span>16:15</span></p>
-            <p>Sintomas: <span>No está comiendo</span></p><button className="button elimnar u-full-width">Eliminar
-              ×</button>
-          </div>
-          <div className="cita">
-            <p>Mascota: <span>Nina</span></p>
-            <p>Dueño: <span>Martin</span></p>
-            <p>Fecha: <span>2021-08-05</span></p>
-            <p>Hora: <span>08:20</span></p>
-            <p>Sintomas: <span>Le duele la pierna</span></p><button className="button elimnar u-full-width">Eliminar
-              ×</button>
-          </div>
-          <div className="cita">
-            <p>Mascota: <span>Sifon</span></p>
-            <p>Dueño: <span>Flecha</span></p>
-            <p>Fecha: <span>2023-06-10</span></p>
-            <p>Hora: <span>09:24</span></p>
-            <p>Sintomas: <span>Duerme mucho</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
-          </div>
+          <Cita/>
+          <Cita/>
+          <Cita/>
+          <Cita/>
         </div>
       </div>
     </div>
---
+++ src/Components/Cita.jsx
@@ -0,0 +1,15 @@
+const Cita = () => {
+    return(
+        
+        <div class="cita">
+              <p>Mascota: <span>Blacky</span></p>
+              <p>Dueño: <span>Jere</span></p>
+              <p>Fecha: <span>2023-05-08</span></p>
+              <p>Hora: <span>16:15</span></p>
+              <p>Sintomas: <span>No está comiendo</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
+        </div>
+    
+    )
+}
+
+export default Cita
