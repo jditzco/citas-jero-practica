6fffa85bef3b1e742c8fcdc3372b75ac86ce39c3
DarkApple55 <axelcy@gmail.com>
2023-06-11 17:50:42 -0300

    puse las stylesheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import Form from './Components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <h1>ADMINISTRADOR DE PACIENTES</h1>
