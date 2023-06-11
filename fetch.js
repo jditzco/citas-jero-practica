0f6b6185468cf3eaa3d12d8a0e0d590da9ff0055
DarkApple55 <axelcy@gmail.com>
2023-06-11 19:28:27 -0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,9 +1,16 @@
-// acordarse de importar useEffect!
-useEffect(() => {
-    const logDatos = async() => {
-      const response = await fetch('https://dummyjson.com/products')
-      const data = await response.json()
-      console.log(data)
-    }
-    logDatos()
-}, [])
+// // acordarse de importar useEffect!
+// useEffect(() => {
+//     const logDatos = async() => {
+//       const response = await fetch('https://dummyjson.com/products')
+//       const data = await response.json()
+//       console.log(data)
+//     }
+//     logDatos()
+// }, [])
+
+// console.clear()
+
+// .map
+// console.clear()
+
+[ {nombre: "juan", edad: 15}, {nombre: "pedro", edad: 25} ].map(({ nombre }) => console.log("hola " + nombre))
